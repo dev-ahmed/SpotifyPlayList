afeace04bf25c0ba474d789b27baebe97fa85a4b
Ahmed <dev.ahmednabil@gmail.com>
2020-03-13 16:56:42 +0200

    init app

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, Modal} from 'react-native';
+import PropTypes from 'prop-types';
+
+const App = ({}) => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+App.propTypes = {};
+export default React.memo(App);
